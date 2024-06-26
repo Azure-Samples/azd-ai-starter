a6ef73f448324cc7892c80b622fa623b3ec3afe2
Liang Wang <lianw@microsoft.com>
2024-06-26 12:58:17 +0800

    Fix Typo

--- .github/validator/validator.py
+++ .github/validator/validator.py
@@ -66,16 +66,19 @@ class Signs:
     WARNING = ":warning:"
 
 
-final_result_format = "# AI Gallery Standard Validation: {{result}} \n The Definition of Done of the AI-Gallery repo can be found [Here]({link}). \n {{message}}".format(
-    link=details_help_link)
+how_to_fix = "## <i>How to fix?</i>\n<b>The full Definition of Done of the AI-Gallery template and fix approached can be found [HERE]({detail_link}).</b>".format(
+    detail_link=details_help_link)
+
+final_result_format = "# AI Gallery Standard Validation: {{result}} \n{{message}}\n\n{end_message}".format(
+    link=details_help_link, end_message=how_to_fix)
 
 
 class ItemResultFormat:
     PASS = "<details><summary>{sign} <b>{{message}}</b>.</summary></details>".format(
         sign=Signs.CHECK)
-    FAIL = "<details><summary>{sign} <b>{{message}}</b>. <a href={detail_link}>[Detail]</a></summary>\n\n{{detail_messages}}\n\n</details>".format(
+    FAIL = "<details><summary>{sign} <b>{{message}}</b>. <a href={detail_link}>[How to fix?]</a></summary>\n\n{{detail_messages}}\n\n</details>".format(
         sign=Signs.BLOCK, detail_link=details_help_link)
-    WARNING = "<details><summary>{sign} <b>{{message}}</b>. <a href={detail_link}>[Detail]</a></summary>\n\n{{detail_messages}}\n\n</details>".format(
+    WARNING = "<details><summary>{sign} <b>{{message}}</b>. <a href={detail_link}>[How to fix?]</a></summary>\n\n{{detail_messages}}\n\n</details>".format(
         sign=Signs.WARNING, detail_link=details_help_link)
 
 
